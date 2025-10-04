dc6d05cbe39de9f3a49ceaa0190f189b10ddcae9
Rikuto <mikadorikuto@gmail.com>
2025-10-04 22:47:22 +0900

    add some code

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+      // this.counter++;
+    },
+    remove() {
+      this.counter = this.counter - 1;
+      // this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
