93d04a4efb7c24848abed85799130647e1d2d901
Rikuto <mikadorikuto@gmail.com>
2025-10-04 23:55:32 +0900

    add some code

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     };
   },
   methods: {
-    add() {
-      this.counter = this.counter + 1;
+    confirmInput() {
+      this.confirmedName = this.name + ' Mikado';
+    },
+    submitForm(event) {
+      // Prevent default form submission behavior (page reload)
+      // event.preventDefault();
+      alert('Submitted!!');
+    },
+    setName(event, lastName) {
+      // event.target is a vanilla JavaScript DOM element
+      // this.name = event.target.value;
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
       // this.counter++;
     },
-    remove() {
-      this.counter = this.counter - 1;
+    remove(num) {
+      this.counter = this.counter - num;
       // this.counter--;
     }
   }
